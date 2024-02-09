476fd415c79bda92cfc857bdd1f8e8d99dd5f256
YojanEstivenPolania <159499142+YojanEstivenPolania@users.noreply.github.com>
2024-02-08 20:20:41 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,13 @@
+
+package holamundo;
+
+/**
+ *
+ * @author Yojan Estiven Polania 20232218112
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println(" Hola mundo ");
+    }
+}
